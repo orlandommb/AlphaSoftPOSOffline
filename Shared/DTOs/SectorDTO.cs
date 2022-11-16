23061c4e120ff98c296f89431c64381bb71c5a85
Orlando Manuel Martinez Batista <12052916+orlandommb@users.noreply.github.com>
2022-11-16 12:52:50 -0400

    Update to .NET 6

--- Shared/DTOs/SectorDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.DTOs
-{
-    public class SectorDTO
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public decimal MontoDelivery { get; set; }
-    }
-}
